e983794d71d51dfd6d28fc47cb4f0db7cb2608ab
Jaden-Reklaw <jwalker117@gmail.com>
2024-10-27 21:10:06 -0500

    cleaned file to load logs.txt and removed used functions

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
 use std::fs;
-use std::io::Error;
 
 fn main() {
 
-    // let text = fs::read_to_string("logs.txt").expect("Unable to read file");
-    // println!("Text: {:#?}", text);
-
-    match divide(81.0, 0.0) {
-        Ok(result) => println!("Result: {}", result),
-        Err(e) => println!("Error: {}", e)
+    match fs::read_to_string("logs.txt") {
+        Ok(content) => {
+            println!("File content: {}", content);
+        },
+        Err(error) => {
+            println!("Error reading file: {}", error);
+        }
     }
-}
-
-//Demonstration of error handling using division
-fn divide(x: f64, y: f64) -> Result<f64, Error> {
-    if y == 0.0 {
-        Err(Error::other("Division by zero"))
-    } else {
-        Ok(x / y)
-    } 
+    
 }
