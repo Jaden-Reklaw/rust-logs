898916908862d1b7a733c64e8681fca946accf22
Jaden-Reklaw <jwalker117@gmail.com>
2024-10-28 20:45:37 -0500

    finished extract_errors method, but still have a mysterious error with lifetimes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
-use std::fs;
+use core::error;
+use std::{f32::consts::E, fs, result};
+
+
+fn extract_errors(text: &str) -> Vec<&str> {
+    text.split('\n')
+        .filter(|line| line.starts_with("ERROR"))
+        .collect()
+}
 
 fn main() {
 
+    let mut errors_log = Vec::new();
+
     match fs::read_to_string("logs.txt") {
         Ok(content) => {
-            println!("File content: {}", content);
+            errors_log = extract_errors(content.as_str());
+            println!("Errors: {:#?}", errors_log);
         },
         Err(error) => {
             println!("Error reading file: {}", error);
         }
     }
+
+    println!("Errors: {:#?}", errors_log);
     
 }
