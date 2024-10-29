d96b6c9ec19bd883d58a5bb2cac7f3fbb5431eeb
Jaden-Reklaw <jwalker117@gmail.com>
2024-10-28 22:02:24 -0500

    did it with the try operator which is a question mark in rust.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::io::Error;
 
 
 fn extract_errors(text: &str) -> Vec<&str> {
@@ -9,9 +10,9 @@ fn extract_errors(text: &str) -> Vec<&str> {
 }
 
 //You can return something from main
-fn main() {
-    let text = fs::read_to_string("logs.txt").expect("Error reading file");
+fn main() -> Result<(), Error> {
+    let text = fs::read_to_string("logs.txt")?;
     let errors_log = extract_errors(text.as_str());
-    fs::write("errors.txt", errors_log.join("\n"))
-    .expect("Error writing file");
+    fs::write("errors.txt", errors_log.join("\n"))?;
+    Ok(())
 }
